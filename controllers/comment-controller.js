18c96b48025f93c6762eec9b2229370b508e3ffb
AshisPatel <ashis.n.patel@gmail.com>
2021-10-13 16:52:49 -0500

    Add post / delete comment routes - Pizza model is broken need to see what happened

---
+++ controllers/comment-controller.js
@@ -0,0 +1,46 @@
+const Comment = require('../models');
+
+const commentController = {
+    // add comment to a specific pizza 
+    addComment({ params, body }, res) {
+        console.log(body);
+        // Creating the comment in the comment document
+        Comment.create(body)
+            // the comment, then has an _id that mongo creates, we pass that on to the next function
+            .then(({ _id }) => {
+                return Pizza.findOneAndUpdate(
+                    // This finds the right pizza based on the params.id that we pass in 
+                    { _id: params.pizzaId },
+                    // We are adding this specific comment's id to the comments array in the Pizza document 
+                    // the $push method is adding data to the pre-existing array of comments in the Pizza document
+                    // all Mongo-DB methods start with a '$' to distinguish them
+                    // This comments section is also a 'subdocument' of the Pizza document
+                    { $push: { comments: _id } },
+                    { new: true }
+                )
+            })
+            .then(dbPizzaData => {
+                dbPizzaData ?
+                    res.status(200).json(dbPizzaData) :
+                    res.status(404).json({ message: 'No pizza found with this id!' })
+            })
+            .catch(err => res.status(500).json(err));
+    },
+    // delete comment
+    deleteComment({ params }, res) {
+        // Find one and delete returns the object that is deleted, so we can pass it on in the next promise. 
+        Comment.findOneAndDelete({ _id: params.commentId })
+            .then(deletedComment => {
+                deletedComment ?
+                    res.status(200).json(deletedComment) :
+                    (Pizza.findOneAndUpdate(
+                        { _id: params.pizzaId },
+                        { $pull: { comments: params.commentId } },
+                        { new: true }
+                    ))
+            })
+            .catch(err => res.status(500).json(err));
+    }
+}
+
+module.exports = commentController;
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require('mongoose');
 
-
 const PizzaSchema = new Schema({
     pizzaName: {
         type: String
@@ -14,12 +13,34 @@ const PizzaSchema = new Schema({
     },
     size: {
         type: String,
-        default:'Large'
+        default: 'Large'
     },
     // Indicates an array as a datatype instead of using type: Array
-    toppings: []
+    toppings: [],
+
+    // We are telling this 'parent' model to expect an item of type Array, that is coming from another model (ObjectId) and also the name of the model
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+    {
+        // this is set to enable the use of virtuals for our model
+        toJSON: {
+            virtuals: true
+        },
+        // This id is a virtual that is returned by mongoose that we do not need
+        id: false
+    }
+);
+
+// Get total number of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
 });
 
 const Pizza = model('Pizza', PizzaSchema);
 
-module.exports = Pizza; 
+module.exports = Pizza;
