c1da9c7743da22e9c70c60d32b229ef818e521c0
AP <ashis.n.patel@gmail.com>
2021-10-13 21:40:22 -0500

    Populate comment info in getAllPizza /getPizzaById functions, with other minor fixes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,10 +1,21 @@
-const { Pizza } =require("../models");
+const { Pizza } = require("../models");
 
 const pizzaController = {
 
     // get al pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+        // We are calling the populate method here to populate all the comment information in the comments key of the Pizza document.
+        // Otherwise, all we get is the id. 
+        // The '-' in the select removes the __v of the comments document
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            // This select '-' method allows us to ignore the __v section of the Pizza document
+            .select('-__v')
+            // This sort with the negative for id, will return our pizzas in descending order, meaning newest pizza first 
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -15,6 +26,11 @@ const pizzaController = {
     // req has been destructed to only grab the params object
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbPizzaData => {
                 dbPizzaData ?
                     res.json(dbPizzaData) :
@@ -57,4 +73,4 @@ const pizzaController = {
     // Side note: the update and delete both have mongoose alternatives of xOne / xMany, but they do not return any data. 
 };
 
-module.exports = pizzaController; 
+module.exports = pizzaController;
